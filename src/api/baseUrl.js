d3eb7753feb3ef0fd7c2c936d06409d9468d0427
sundusardian <sundusardian@gmail.com>
2022-05-20 03:08:07 +0700

    fixing baseUrl

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 // create baseUrl
 export const baseUrl = axios.create({
-  baseURL: "http://api.github.com",
+  baseURL: "https://api.github.com",
   timeout: 5000,
 });
