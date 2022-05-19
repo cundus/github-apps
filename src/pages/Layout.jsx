da1c7bb81677e4401498d6752b3e4542ca03e5ea
sundusardian <sundusardian@gmail.com>
2022-05-20 03:11:23 +0700

    adding footer to layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Nav } from "../components";
+import { Footer, Nav } from "../components";
 
 const Layout = () => {
   return (
     <div>
       <Nav />
-      <div className="mt-4">
+      <div className="mt-4 min-h-[100vh]">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
