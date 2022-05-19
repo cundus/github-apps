06e930d14d0db67aa05c958f53a4f15bdb66bf84
sundusardian <sundusardian@gmail.com>
2022-05-20 02:51:31 +0700

    fixing User Page

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { getUser, getUserRepos } from "../../api/logic/ApiLogic";
-import { Card, CardRepos } from "../../components";
+import { Card, CardRepos, Spinner } from "../../components";
 import { GrLocation } from "react-icons/gr";
 
 const User = () => {
   const params = useParams();
   const { id } = params;
-
+  const [loading, setLoading] = React.useState(false);
   const [repos, setRepos] = React.useState([]);
   const [user, setUser] = React.useState({});
 
   React.useEffect(() => {
     const getData = async () => {
+      setLoading(true);
       const res = await getUser(id);
       // console.log(res);
       setUser(res);
+      setLoading(false);
     };
 
     const getRepos = async () => {
@@ -28,6 +30,14 @@ const User = () => {
     getRepos();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-40">
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
     <div className="container mx-auto p-10">
       <div className="flex border-2 space-x-5 border-gray-400 p-5 rounded-md justify-evenly max-w-[30rem] mx-auto">
@@ -37,11 +47,13 @@ const User = () => {
             <h1 className="text-3xl font-bold">{id}</h1>
             <p className="text-gray-600">{user.name}</p>
           </div>
-          <div>
-            <span className="flex items-center gap-2 ">
-              <GrLocation /> {user.location}
-            </span>
-          </div>
+          {user.location && (
+            <div>
+              <span className="flex items-center gap-2 ">
+                <GrLocation /> {user.location}
+              </span>
+            </div>
+          )}
         </div>
       </div>
       <div className=" mt-8 space-y-5">
