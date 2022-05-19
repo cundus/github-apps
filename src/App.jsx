49ea3d93cb6ce653c933c8a754729884464f4dd9
sundusardian <sundusardian@gmail.com>
2022-05-20 03:39:07 +0700

    adding google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Layout, Search, User } from "./pages";
 import "./App.css";
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-Z3VQ187MKG";
 
 const App = () => {
+  ReactGA.initialize(TRACKING_ID);
+
+  React.useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   // configure react-router
   return (
     <BrowserRouter>
