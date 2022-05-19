900f8b085bb5674e967e36f895072ef36235f3c9
sundusardian <sundusardian@gmail.com>
2022-05-20 02:51:12 +0700

    done create index to export Components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Nav } from "./Nav";
+export { default as SearchBar } from "./SearchBar";
+export { default as Card } from "./Card";
+export { default as Footer } from "./Footer";
+export { default as CardRepos } from "./CardRepos";
+export { default as Spinner } from "./Spinner";
