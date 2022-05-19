1f8835775c1f3503b3aa8cef3e20ea77f68ffe0d
sundusardian <sundusardian@gmail.com>
2022-05-20 02:52:24 +0700

    done create index to export from Pages Component

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Layout } from "./Layout";
+export { default as User } from "./User/User";
+export { default as Home } from "./Home/Home";
+export { default as Search } from "./Search/Search";
